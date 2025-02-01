c0eeec07d09f2e99d404f24dbc062cc1da425718
Duane Northcutt <duane.northcutt@gmail.com>
2025-02-01 14:21:52 -0800

    Fist cut.

--- scripts/orag.py
+++ scripts/orag.py
@@ -13,20 +13,34 @@ from string import Template
 
 import faiss
 from langchain_huggingface import HuggingFaceEmbeddings
-#from langchain_community.embeddings import HuggingFaceEmbeddings
 import ollama
+
+####import pdb  #### TMP TMP TMP
 
 
 LOG_LEVEL = "WARNING"
 
-# name of Ollama model to use
-LLM_MODEL = "deepseek-r1:1.5b"
+# name of Ollama LLM to use
+DEF_LLM_NAME = "deepseek-r1:1.5b"
 
-# path to docs that define the knowledge base
-DOCS_PATH = "/home/jdn/Code/LLM_Experiments/assets/"
+# name of Huggingface Embedding Model to use
+DEF_EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"
 
-# instruct model to respond based only on the retrieved context
-PROMPT_TEMPLATE = Template("""
+
+class SimpleRetriever():
+    def __init__(self, documents, index, embeddingModel):
+        self.documents = documents
+        self.index = index
+        self.embeddingModel = embeddingModel
+
+    def retrieve(self, query, k=3):
+        queryEmbedding = self.embeddingModel.embed_query(query)
+        distances, indices = self.index.search(np.array([queryEmbedding]).astype('float32'), k)
+        return [self.documents[i] for i in indices[0]]
+
+class RetrievalAugmentedGenerator():
+    # instruct model to respond based only on the retrieved context
+    PROMPT_TEMPLATE = Template("""
 Use ONLY the context below.
 If unsure, say "I don't know".
 Keep answers under 4 sentences.
@@ -36,23 +50,6 @@ Question: $question
 Answer:
 """)
 
-USER_QUESTIONS = (
-    "What are the key features of DeepSeek R1?",
-    "What is your favorite color?",
-    "What is the meaning of life?")
-
-
-class SimpleRetriever():
-    def __init__(self, index, embeddingsModel):
-        self.index = index
-        self.embeddingsModel = embeddingsModel
-    
-    def retrieve(self, query, k=3):
-        queryEmbedding = self.embeddingsModel.embed(query)
-        distances, indices = self.index.search(np.array([queryEmbedding]).astype('float32'), k)
-        return [documents[i] for i in indices[0]]
-
-class RetrievalAugmentedGenerator():
     @staticmethod
     def _loadDocuments(directory):
         documents = []
@@ -62,24 +59,26 @@ class RetrievalAugmentedGenerator():
                     documents.append(file.read())
         return documents
 
-    def __init__(self, model, promptTemplate):
-        self.promptTemplate = promptTemplate
+    def __init__(self, docsPath,
+                 embeddingModelName=DEF_EMBEDDING_MODEL_NAME,
+                 modelName=DEF_LLM_NAME):
+        self.model = modelName
+        self.client = ollama.Client()
 
-    def setKnowledgeBase(self, docsPath):
         self.docs = RetrievalAugmentedGenerator._loadDocuments(docsPath)
 
-        # init the embeddings model and generate embeddings for all documents
-        self.embeddingsModel = HuggingFaceEmbeddings()
-        #docEmbeddings = [self.embeddingsModel.embed(doc) for doc in self.docs]
-        docEmbeddings = self.embeddingsModel.embed_documents(self.docs)
+        # init the embedding model and generate embeddings for all documents
+        self.embeddingModel = HuggingFaceEmbeddings(model_name=embeddingModelName)
+        docEmbeddings = self.embeddingModel.embed_documents(self.docs)
         self.docEmbeddings = np.array(docEmbeddings).astype('float32')
 
         # create FAISS index and add document embeddings to the index
         self.index = faiss.IndexFlatL2(self.docEmbeddings.shape[1])  # use L2 distance metric
-        self.index.add(docEmbeddings)
+        ####pdb.set_trace()
+        self.index.add(self.docEmbeddings)
 
         # create retriever based on user queries to fetch most relevant documents
-        self.retriever = SimpleRetriever(self.index, self.embeddingsModel)
+        self.retriever = SimpleRetriever(self.docs, self.index, self.embeddingModel)
 
     def answerQuestion(self, question):
         # retrieve relevant context from the knowledge base
@@ -89,16 +88,29 @@ class RetrievalAugmentedGenerator():
         combinedContext = "n".join(context)
     
         # generate an answer with the model, using the combined context
-        response = ollama.generate(promptTemplate.substitute(context=combinedContext, question=question))
-        return response.strip()
+        fullPrompt = RetrievalAugmentedGenerator.PROMPT_TEMPLATE.substitute(context=combinedContext, question=question)
+        print("PROMPT START --------------------")
+        print(fullPrompt)
+        print("PROMPT END -------------------")
+        answer = self.client.generate(model=self.model, prompt=fullPrompt)
+        return answer.response
 
 if __name__ == "__main__":
     logging.basicConfig(level=LOG_LEVEL)
-    rag = RetrievalAugmentedGenerator(LLM_MODEL, PROMPT_TEMPLATE)
-    rag.setKnowledgeBase(DOCS_PATH)
+
+    # path to docs that define the knowledge base
+    DOCS_PATH = "/home/jdn/Code/LLM_Experiments/assets/"
+
+    # list of questions to ask
+    USER_QUESTIONS = (
+        "How do I use a namespace?",
+        "How does auto type declaration work?",
+        "What is your favorite color?")
+
+    rag = RetrievalAugmentedGenerator(DOCS_PATH)
 
     for i, question in enumerate(USER_QUESTIONS):
         print(f"Question #{i}: {question}")
-        answer = rag.answerQuery(question)
+        answer = rag.answerQuestion(question)
         print(f"Answer #{i}: {answer}")
-
+        print("--------------------")
