95453911139fc0c5b7a2893844927562068ff158
Duane Northcutt <duane.northcutt@gmail.com>
2025-02-03 12:28:33 -0800

    Return thoughts and answer parts.

--- src/rag/RetrievalAugmentedGeneration.py
+++ src/rag/RetrievalAugmentedGeneration.py
@@ -9,7 +9,6 @@
 ####  * make generalized retriever object and subclass with FAISS and ChromaDB
 ####    - and try with both vector stores
 ####    - compare performance of FAISS vs. Chroma for Vector Store
-####  * write an interactive tools that uses this as a library
 
 
 import logging
@@ -27,39 +26,43 @@ import pdb  #### TMP TMP TMP
 
 
 class RetrievalAugmentedGeneration():
-    def __init__(self, embeddingsStore, retriever, modelName, globalContext):
+    def __init__(self, embeddingsStore, modelName, globalContext=""):
         self.embeddingsStore = embeddingsStore
-        self.retriever = retriever
         self.model = modelName
+        self.globalContext = globalContext
         self.client = ollama.Client()
 
-    def answerQuestion(self, question):
+    def answerQuestion(self, question, numDocs=None, threshold=None):
         # retrieve relevant context from the knowledge base/vector store
-        docs = self.vectorStore.similarity_search(question, k=kVal)
-        #### TODO add thresholds
-        #### N.B. ChromaDB uses cosine distance, so lower means more similar
-        #### THRESH = 0.3
-#        results = self.vectorStore.similarity_search_with_score(question, k=kVal)
-#        filteredResults = [doc for doc, score in results if score <= THRESH]
-        #### alternatively
-#        retriever = self.vectorStore.as_retriever(search_type='similarity_score_threshold', search_kwargs={'score_threshold': 0.21, 'k': 5}')
-        context = "\n".join([doc.page_content for doc in docs])
+        context = self.embeddingsStore.getContext(question)
     
         # combine contexts and query
         #### FIXME see if I can do this easier with an f-string
         prompt = f"Context: {context}\n\nQuestion: {question}\n\nAnswer:"
         fullPrompt = Template("""
-$globalContext
+$gcontext
 
 Context: $context
 Question: $question
 Answer:
-""").substitute(globalContext=globalContext, context=context, question=question)
+""").substitute(gcontext=self.globalContext, context=context, question=question)
     
         # generate an answer with the model, using the combined context
         #### FIXME split into thoughts and answer
         response = self.client.generate(model=self.model, prompt=fullPrompt)
-        answer = re.sub(r'<think>.*?</think>', '', response['response'], flags=re.DOTALL)
+#        answer = re.sub(r'<think>.*?</think>', '', response['response'], flags=re.DOTALL)
+        pattern = f"({re.escape('<think>')}.*?{re.escape('</think>')})"
+        parts = re.split(pattern, response['response'], maxsplit=1, flags=re.DOTALL)
+        if len(parts) == 3:
+            thoughts = parts[1]
+            answer = parts[2]
+        elif len(parts) == 1:
+            thoughts = None
+            answer = parts[0]
+        else:
+            logging.error(f"Confused response: {parts}")
+            thoughts = None
+            answer = None
         return thoughts, answer
 
 if __name__ == "__main__":
