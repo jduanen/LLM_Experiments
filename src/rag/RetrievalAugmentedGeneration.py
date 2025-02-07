b4c4d6d582a175fb8089d6cd81e9fc93011f723f
Duane Northcutt <duane.northcutt@gmail.com>
2025-02-06 17:03:18 -0800

    Added timings. Fixed logging.

--- src/rag/RetrievalAugmentedGeneration.py
+++ src/rag/RetrievalAugmentedGeneration.py
@@ -10,11 +10,11 @@
 ####    - and try with both vector stores
 ####    - compare performance of FAISS vs. Chroma for Vector Store
 
-
 import logging
 import numpy as np
 import os
 from string import Template
+import time
 
 #import faiss
 import ollama
@@ -24,35 +24,39 @@ import pdb  ## pdb.set_trace()  #### TMP TMP TMP
 #from rag.retrievers import pdfRetriever, txtRetriever
 
 
+logger = logging.getLogger(__name__)
+
+
 class RetrievalAugmentedGeneration():
     def __init__(self, embeddingsStore, modelName, globalContext=""):
         self.embeddingsStore = embeddingsStore
         self.model = modelName
         self.globalContext = globalContext
         self.client = ollama.Client()
+        logger.debug("Create RAG")
 
     def answerQuestion(self, question, numDocs=None, threshold=None):
         # retrieve relevant context from the knowledge base/vector store
+        logger.debug("Answer Question")
+        metadata = {}
+        startTime = time.time()
         context = self.embeddingsStore.getContext(question)
-    
-        # combine contexts and query
-        #### FIXME see if I can do this easier with an f-string
-        prompt = f"Context: {context}\n\nQuestion: {question}\n\nAnswer:"
-        fullPrompt = Template("""
-$gcontext
+        metadata['getContextTime'] = time.time() - startTime
+        metadata['context'] = context
 
-Context: $context
-Question: $question
-Answer:
-""").substitute(gcontext=self.globalContext, context=context, question=question)
-    
         # generate an answer with the model, using the combined context
+        fullPrompt = f"""
+Context: {self.globalContext} {context}
+Question: {question}
+Answer:
+"""
+        metadata['context'] = context
+        startTime = time.time()
         response = self.client.generate(model=self.model, prompt=fullPrompt)
-        return response
+        metadata['generateTime'] = time.time() - startTime
+        return response, metadata
 
 if __name__ == "__main__":
-    logging.basicConfig(level=LOG_LEVEL)
-
     # define whether thoughts are to be printed in answers
     PRINT_THOUGHTS =  True  #False  # True
 
