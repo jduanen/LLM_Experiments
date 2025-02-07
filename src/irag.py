c4dc8047e5ad414828298f3712bfafedca0f9cd4
Duane Northcutt <duane.northcutt@gmail.com>
2025-02-06 17:02:09 -0800

    Added more timing stats. Adjusted defaults.

--- src/irag.py
+++ src/irag.py
@@ -13,6 +13,7 @@ import logging
 import os
 import re
 import sys
+import time
 import yaml
 
 import pdb  ## pdb.set_trace()  #### TMP TMP TMP
@@ -23,19 +24,26 @@ from rag.EmbeddingsStore import EmbeddingsStore
 
 DEF_LOG_LEVEL = "WARNING"
 
-DEF_LLM_NAME = "deepseek-r1:1.5b"
+DEF_LLM_NAME = "deepseek-r1:8b"
 
-DEF_CHUNK_SIZE = 2000
-DEF_CHUNK_OVERLAP = 0
+DEF_CHUNK_SIZE = 1000
+DEF_CHUNK_OVERLAP = 500
+
+DEF_EMBD_MODEL = "all-mpnet-base-v2"
 
 DEF_OUTPUT_FORMAT = "HUMAN"
 
+DEF_TEMPERATURE = 0.4
+DEF_NUM_SENTENCES = 10
+DEF_PERSONA = "physicist"  # "programmer"
+
 # instruct model to respond based only on the retrieved context
-DEF_GLOBAL_CONTEXT = """
-You are an experienced programmer, speaking to another experienced programmer.
+DEF_GLOBAL_CONTEXT = f"""
+You are an experienced ${DEF_PERSONA}, speaking to another experienced ${DEF_PERSONA}.
 Use ONLY the context below.
 If unsure, say "I don't know".
-Keep answers under 8 sentences.
+Keep answers under {DEF_NUM_SENTENCES} sentences.
+[temperature: {DEF_TEMPERATURE}]
 """
 
 DEFAULTS = {
@@ -43,7 +51,7 @@ DEFAULTS = {
     "chunkSize": DEF_CHUNK_SIZE,
     "confFile": ".rag.conf",
     "docPath": None,
-    "embeddingModel": "all-mpnet-base-v2",
+    "embeddingModel": DEF_EMBD_MODEL,
     "globalContext": DEF_GLOBAL_CONTEXT,
     "logLevel": DEF_LOG_LEVEL,
     "logFile": None,
@@ -52,18 +60,16 @@ DEFAULTS = {
     "outputFormat": DEF_OUTPUT_FORMAT,
     "printThoughts": False,
     "query": None,
-    "saveEmbeddingsPath": False,
+    "saveEmbeddingsPath": None,
     "similarity": "Cosine",
     "threshold": None,
-    "useEmbeddingsPath": False,
+    "useEmbeddingsPath": None,
     "vectorStore": "ChromaDB",
     "verbose": False
 }
 
 
 def getOpts():
-    logging.basicConfig(level=DEF_LOG_LEVEL)
-
     usage = f"Usage: {sys.argv[0]} [-v] [-c <confFile>] [-L <logLevel>] [-l <logFile>] [-m <model>] \
 [-q <query>] [-g <globalContext>] [-p <printThoughts>] [-k <numRetrieves>] [-t <threshold>] \
 [-E <saveEmbeddingsPath>] [-u <useEmbeddingsPath>] \
@@ -115,7 +121,7 @@ def getOpts():
         help="Path to directory for (.txt and .pdf) context files")
     embeddingsGroup.add_argument(
         "-e", "--embeddingModel", action="store", type=str,
-        choices=["all-mpnet-base-v2", "????"],
+        choices=["all-mpnet-base-v2", "all-MiniLM-L6-v2"],
         help="Name of the HuggingFace sentence-transformer embedding model to use")
     embeddingsGroup.add_argument(
         "-C", "--chunkSize", action="store", type=int,
@@ -144,14 +150,14 @@ def getOpts():
     conf = {'cli': cliOpts, 'confFile': {}, 'config': {}}
     if cliOpts['confFile']:
         if not os.path.exists(cliOpts['confFile']):
-            logging.error(f"Invalid configuration file: {cliOpts['confFile']}")
+            print(f"ERROR: Invalid configuration file: {cliOpts['confFile']}")
             exit(1)
         with open(cliOpts['confFile'], "r") as confsFile:
             confs = list(yaml.load_all(confsFile, Loader=yaml.Loader))
             if len(confs) >= 1:
                 conf['confFile'] = confs[0]
                 if len(confs) > 1:
-                    logging.warning(f"Multiple config docs in file. Using the first one")
+                    print(f"WARNING: Multiple config docs in file. Using the first one")
 
     # options precedence order: cmd line -> conf file -> defaults
     #   cliOpts: cmd line options
@@ -172,22 +178,22 @@ def getOpts():
         json.dump(conf, sys.stdout, indent=4, sort_keys=True)
         print("")
 
-    if conf['logFile']:
+    if conf['logFile'] and (conf['logFile'] != '-'):
         logging.basicConfig(filename=conf['logFile'], level=conf['logLevel'])
     else:
         logging.basicConfig(level=conf['logLevel'])
+    logging.debug(f"Logging to {conf['logFile']} at level {conf['logLevel']}")
 
     # check for consistency among switches
     if conf['useEmbeddingsPath']:
         for k in ('docPath', 'embeddingModel', 'chunkOverlap', 'chunkSize'):
             if not conf.get(k):
                 logging.warning(f"Using saved embeddings, ignoring {k}")
-
     return conf
 
 def run(options):
     def handleResponse():
-        # split up response into it's parts
+        #### FIXME use match and get the two parts without the delimiters
         pattern = f"({re.escape('<think>')}.*?{re.escape('</think>')})"
         parts = re.split(pattern, response['response'], maxsplit=1, flags=re.DOTALL)
         if len(parts) == 3:
@@ -203,63 +209,75 @@ def run(options):
         stats = {
             'totalDuration': response['total_duration'],
             'loadDuration': response['load_duration'],
-            'promptEvalCount': response['prompt_eval_count'],
+            'promptEvalCount': int(response['prompt_eval_count']),
             'promptEvalDuration': response['prompt_eval_duration'],
             'promptEvalRate': (response['prompt_eval_count'] * 1000000000) / response['prompt_eval_duration'],
-            'evalCount': response['eval_count'],
+            'evalCount': int(response['eval_count']),
             'evalDuration': response['eval_duration'],
             'evalRate': (response['eval_count'] * 1000000000.0) / response['eval_duration'],
         }
 
-        #### TODO figure out how to get the full prompt (i.e., context + query) and print it too
-
-        print(f">>> {options['outputFormat']}")
         if options['outputFormat'] == "HUMAN":
             print(f"\nQuestion: {query}")
+            if options['verbose'] > 1:
+                print(f"\nContext:\n{metadata['context']}")
             if options['printThoughts']:
-                print("----------------")
-                print(f"\nThoughts: {thoughts}")
+                print(f"\nThoughts:\n{thoughts}")
             print(f"\nAnswer: {answer}")
             if options['verbose'] > 1:
-                print("\nStats:")
+                print("\nPerformance:")
+                print(f"    Setup Embs:  {metadata['embStoreSetupTime']:.2f} secs")
+                print(f"    Get Context: {metadata['getContextTime']:.2f} secs")
+                print(f"    Generate:    {metadata['generateTime']:.2f} secs")
+                print("\nLLM Generate Stats:")
                 print(f"    Total Duration:       {(stats['totalDuration'] / 1000000000.0):.2f} secs")
                 print(f"    Load Duration:        {(stats['loadDuration'] / 1000000.0):.2f} msecs")
-                print(f"    Prompt Eval Tokens:   {stats['promptEvalCount']:.2f} tokens")
+                print(f"    Prompt Eval Tokens:   {int(stats['promptEvalCount'])} tokens")
                 print(f"    Prompt Eval Duration: {(stats['promptEvalDuration'] / 1000000.0):.2f} msecs")
                 print(f"    Prompt Eval Rate:     {stats['promptEvalRate']:.2f} tokens/sec")
-                print(f"    Eval Count:           {stats['evalCount']:.2f} tokens")
+                print(f"    Eval Count:           {int(stats['evalCount'])} tokens")
                 print(f"    Eval Duration:        {(stats['evalDuration'] / 1000000000.0):.2f} secs")
                 print(f"    Eval Rate:            {stats['evalRate']:.2f} tokens/sec")
+                totalTime = metadata['embStoreSetupTime'] + metadata['getContextTime'] + metadata['generateTime']
+                print(f"\nTotal time: {totalTime:.2f} secs")
             print("\n")
         elif options['outputFormat'] == "JSON":
-            outDict = {'query': query, 'answer': answer, 'reason': response.done_reason}
+            outDict = {'query': query, 'context': metadata['context'], 'answer': answer, 'reason': response.done_reason}
             if options['printThoughts']:
                 outDict['thoughts'] = thoughts
             if options['verbose'] > 1:
                 outDict['stats'] = stats
+            outDict['embStats'] = {'embStoreSetupTime': metadata['embStoreSetupTime'],
+                                   'getContextTime': metadata['getContextTime'],
+                                   'generateTime': metadata['generateTime']}
             print(json.dumps(outDict, indent=4, sort_keys=True))
             print("\n")
         else:
             logging.warning(f"Unknown output format: {options['outputFormat']}")
 
-    embeddingsStore = EmbeddingsStore(options['numRetrieves'], options['threshold'])
+    startTime = time.time()
+    embeddingsStore = EmbeddingsStore(options['embeddingModel'], options['numRetrieves'], options['threshold'])
     if options['useEmbeddingsPath']:
         embeddingsStore.useStore(options['useEmbeddingsPath'])
     else:
+        # N.B. if saveEmbeddingsPath is None, then don't persist the vector store
         embeddingsStore.createStore(options['docPath'], options['chunkSize'],
                                     options['chunkOverlap'], options['saveEmbeddingsPath'])
     rag = RetrievalAugmentedGeneration(embeddingsStore, options['model'], options['globalContext'])
+    embStoreSetupTime = time.time() - startTime
 
     query = options['query']
     if query:
-        response = rag.answerQuestion(query)
+        response, metadata = rag.answerQuestion(query)
+        metadata['embStoreSetupTime'] = embStoreSetupTime
         handleResponse()
     else:
         while True:
             query = input("Question: ")
             if not query:
                 break
-            response = rag.answerQuestion(query)
+            response, metadata = rag.answerQuestion(query)
+            metadata['embStoreSetupTime'] = embStoreSetupTime
             handleResponse()
     logging.debug("Exiting")
 
